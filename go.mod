58ea4b6e027d1855612b01c8b05cb43d41584fde
Amar Tabakovic <tabakovicamar1@gmail.com>
2021-10-20 00:10:55 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amartabakovic/go-cat
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		log.Fatal("Currently only one file supported.")
+	}
+
+	if len(args) == 0 {
+		log.Fatal("Please write in a file name.")
+	}
+
+	arg := args[0]
+
+	filerc, err := os.Open(arg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filerc.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(filerc)
+	contents := buf.String()
+
+	newStr := ""
+	iter := 1
+
+	for _, element := range contents {
+		newStr += string("\033[3"+strconv.Itoa(iter)+"m") + string(element)
+
+		if iter == 6 {
+			iter = 0
+		}
+		iter++
+	}
+
+	fmt.Println(newStr)
+}
